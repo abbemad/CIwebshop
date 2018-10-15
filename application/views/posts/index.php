17443c8d51fa29f0e8b151fb5960d9e3b697708d
Damien <abbemad@gmail.com>
2018-10-16 00:36:21 +0200

    DB + posts page + routes for posts + nav links

---
+++ application/views/posts/index.php
@@ -0,0 +1,14 @@
+<h2><?= $title ?></h2>
+
+<?php foreach($posts as $post) : ?>
+
+    <h3><?php echo $post['title']; ?></h3>
+        <small class="post-date">Posted: <?php echo $post['created_at']; ?> </small> 
+        <br>
+            <?php echo $post['body']; ?> 
+            <br>
+                <p><a class="btn btn-default" href="<?php echo site_url('/posts/'.$post['slug']); ?>">
+                        Read more 
+                    </a>
+                </p>
+<?php endforeach; ?>
