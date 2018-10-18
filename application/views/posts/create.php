373ae467871b4189a51dd4666e491cb628da322b
Damien <abbemad@gmail.com>
2018-10-18 02:41:10 +0200

    auto load form

--- application/views/posts/create.php
+++ application/views/posts/create.php
@@ -1,7 +1,8 @@
 <h2><?= $title; ?> </h2>
 
+<?php echo validation_errors(); ?>
+<?php echo form_open('posts/create'); ?>
 
-<form>
   <div class="form-group">
     <label>Title</label>
     <input type="text" class="form-control" name="title" placeholder="Add title">
