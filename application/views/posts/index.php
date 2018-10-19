03a04fd8851fe107bd5f42f1b4ef370c91986143
Damien <abbemad@gmail.com>
2018-10-19 04:29:39 +0200

    show image in posts

--- application/views/posts/index.php
+++ application/views/posts/index.php
@@ -3,7 +3,13 @@
 <?php foreach($posts as $post) : ?>
 
     <h3><?php echo $post['title']; ?></h3>
-        <small class="post-date">Posted: <?php echo $post['created_at']; ?> in <strong> <?php echo $post['name']; ?> </strong> </small> 
+    <div class="row">
+    <div class="col-md-3">
+    <img src="<?php echo site_url(); ?>assets/images/posts/<?php echo $post['post_image']; ?>">
+    </div>
+
+    <div class="col-md-9">
+    <small class="post-date">Posted: <?php echo $post['created_at']; ?> in <strong> <?php echo $post['name']; ?> </strong> </small> 
         <br>
             <?php echo word_limiter($post['body'], 50); ?> 
             <br>
@@ -11,4 +17,8 @@
                         Read more 
                     </a>
                 </p>
+    </div>
+    </div>
+
+ 
 <?php endforeach; ?>
