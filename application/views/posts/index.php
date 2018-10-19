3c41d051e7e35525be1da22467ea1319a7f6925c
Damien <abbemad@gmail.com>
2018-10-19 03:12:30 +0200

    word limiter in posts

--- application/views/posts/index.php
+++ application/views/posts/index.php
@@ -5,7 +5,7 @@
     <h3><?php echo $post['title']; ?></h3>
         <small class="post-date">Posted: <?php echo $post['created_at']; ?> </small> 
         <br>
-            <?php echo $post['body']; ?> 
+            <?php echo word_limiter($post['body'], 50); ?> 
             <br>
                 <p><a class="btn btn-primary" href="<?php echo site_url('/posts/'.$post['slug']); ?>">
                         Read more 
