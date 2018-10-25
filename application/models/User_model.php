fb11ef8b2661eaa7506d6a831e19aa06f6823c11
Damien <abbemad@gmail.com>
2018-10-25 15:51:24 +0200

    insert user in db

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -1,7 +1,18 @@
 <?php 
     class User_model extends CI_Model{
-        public function  __construct(){
-            $this->load->database();
+        public function  __construct($enc_password){
+            // data array
+
+            $data = array(
+                'name' => $this->input->post('name'),
+                'email' => $this->input->post('email'),
+                'username' => $this->input->post('username'),
+                'password' => $enc_password,
+                'zipcode' => $this->input->post('zipcode')
+            );
+            
+            //insert user
+            return $this->db->insert('users', $data);
         }
 
     }
