8fb2201961877922aa728433015be095622fe64c
Damien <abbemad@gmail.com>
2018-11-01 02:18:00 +0100

    nav login logout

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -26,14 +26,18 @@
                     </ul>
 
                     <ul class= "nav navbar-nav navbar-right">
+
+                    <?php if(!$this->session->userdata('logged_in')) : ?>
                         <li><a href="<?php echo base_url(); ?>users/login">Login</a></li>
                         <li><a href="<?php echo base_url(); ?>users/register">Register</a></li>
+                    <?php endif; ?>
 
+                    <?php if(!$this->session->userdata('logged_in')) : ?>
                         <li><a href="<?php echo base_url(); ?>posts/create">Create Post</a></li>
                         <li><a href="<?php echo base_url(); ?>categories/create">Create Category</a></li>
-
                         <li><a href="<?php echo base_url(); ?>users/logout">Logout</a></li>
-                        
+                    <?php endif; ?>
+                    
                     </ul>
                 </div>
             </div>
